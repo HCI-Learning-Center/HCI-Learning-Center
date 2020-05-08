246452c3e6dc510572c22025fd8b39400905843d
Jamila Toaha <jtoahacoding@gmail.com>
2020-05-08 09:37:53 -0400

    added updated tutor array, so more days of calendar filled

--- HCI_Group_ Project/WritingTutorsSchedule.js
+++ HCI_Group_ Project/WritingTutorsSchedule.js
@@ -2,190 +2,7 @@
 
 //  ============================================= This project does not require a backend at the moment. So we will build in our 'database' here and make use of local storage. array of tutors. Each tutor has a select list of appointmsnts available
 
-/**Variable values
-tutors-> name -> appointments -> status: booked/ available /pending //maybe true or false
-tutors-> name -> appointments -> student: name of student who booked appointment
 
-time: new Date(year, month, day, hours, minutes, seconds, milliseconds)
-*/
-
-
-
-//let sampleDate = new Date(2020, 5, 9, 10, 0)
-let today = new Date();
-let sampleMonth = today.getMonth(); //get current Month
-//notice offset: 3 corresponds to April
-let sampleDay = today.getDate(); // get current day of the Month
-
-// console.log(sampleMonth, sampleDay);
-
-let tutors = [
-	{
-		name: 'Jenna Doe',
-		employeeId: 0,
-		appointments: [
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 10, 0),
-					endTime: new Date(2020, sampleMonth, sampleDay, 10, 20),
-					location: 'BH 3129',
-					description: 'Jenna Doe has a major in English Language and Literature. She is here to assist and provide practical tips you can apply to your papers',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 10, 20),
-					endTime: new Date(2020, sampleMonth, sampleDay, 10, 40),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 10, 40),
-					endTime: new Date(2020, sampleMonth, sampleDay, 11, 0),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 2, 0),
-					endTime: new Date(2020, sampleMonth, sampleDay, 2, 20),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 2, 20),
-					endTime: new Date(2020, sampleMonth, sampleDay, 2, 40),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-		],
-	},
-
-	{
-		name: 'John Smith',
-		employeeId: 1,
-		appointments: [
-			{
-				startTime: new Date(2020, sampleMonth, sampleDay, 10, 0),
-				endTime: new Date(2020, sampleMonth, sampleDay, 10, 20),
-				location: 'BH 3129',
-				description: 'John Smith specializes in Creative Writing and would love to be of assistance.',
-				status: 'available',
-				student: '', //insert entire student object here if possible
-			},
-			{
-				startTime: new Date(2020, sampleMonth, sampleDay, 10, 20),
-				endTime: new Date(2020, sampleMonth, sampleDay, 10, 40),
-				location: 'BH 3129',
-				description: 'John Smith specializes in Creative Writing and would love to be of assistance.',
-				status: 'available',
-				student: '', //insert entire student object here if possible
-			},
-			{
-				startTime: new Date(2020, sampleMonth, sampleDay, 10, 40),
-				endTime: new Date(2020, sampleMonth, sampleDay, 11, 0),
-				location: 'BH 3129',
-				description: 'John Smith specializes in Creative Writing and would love to be of assistance.',
-				status: 'available',
-				student: '', //insert entire student object here if possible
-			},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 11, 0),
-					endTime: new Date(2020, sampleMonth, sampleDay, 11, 20),
-					location: 'BH 3129',
-					description: 'John Smith specializes in Creative Writing and would love to be of assistance.',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12, 20),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12, 40),
-					location: 'BH 3129',
-					description: 'John Smith specializes in Creative Writing and would love to be of assistance.',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 1, 0),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12+ 1, 20),
-					location: 'BH 3129',
-					description: 'John Smith specializes in Creative Writing and would love to be of assistance.',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 1, 40),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 2, 0),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 2, 20),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 2, 40),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-		],
-	},
-
-
-	{
-		name: 'Shikamaru Nara',
-		employeeId: 2,
-		appointments: [
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 4, 0),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 4, 20),
-					location: 'BH 3129',
-					description: 'Shikamaru Nara is an expert in drafting academic articles. He can provide  some guidance on the papers you have been putting off.',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 4, 20),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 4, 40),
-					location: 'BH 3129',
-					description: 'Shikamaru Nara is an expert in drafting academic articles. He can provide  some guidance on the papers you have been putting off.',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 5, 0),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 5, 20),
-					location: 'BH 3129',
-					description: 'Shikamaru Nara is an expert in drafting academic articles. He can provide  some guidance on the papers you have been putting off.',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 5, 20),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 5, 40),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-				{
-					startTime: new Date(2020, sampleMonth, sampleDay, 12 + 5, 40),
-					endTime: new Date(2020, sampleMonth, sampleDay, 12 + 6, 0),
-					location: 'BH 3129',
-					description: '',
-					status: 'available',
-					student: '', //insert entire student object here if possible
-				},
-		],
-	}
-]
 
 //console.log(tutors)
 
@@ -387,13 +204,13 @@ function displayBookedAppointments(){
 	let appointmentHTML = ''
   for (let tutor of tutors) {
       for (let [i, appointment] of tutor.appointments.entries()){
-
 		//console.log(tutors)//if the appointment is not available, then do not display it, and move on to next round of the for loop
 			if(appointment.status === 'booked'){
 				appointmentHTML+= `
 				<span>
 				<input class="radioCancelButtonClick" name="canceloption" type="radio" value="employeeId_${tutor.employeeId}_appIndex_${i}"}>
-				<label> <p>Tutor: ${tutor.name}, Time: ${moment(appointment.startTime).format('LT')} - ${moment(appointment.endTime).format('LT')}, Location: ${appointment.location} </p>
+				<label>
+				<p>Tutor: ${tutor.name}, Date & Time: ${moment(appointment.startTime).format('dddd, MMMM Do YYYY, h:mm a')} - ${moment(appointment.endTime).format('LT')}, Location: ${appointment.location} </p>
 				</label>
 				<br></span>`
 			}
